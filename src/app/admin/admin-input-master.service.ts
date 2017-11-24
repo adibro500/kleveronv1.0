9d65430409e9d99b9c7bfa91804d5b5e555f5423
Aditya Ammanabrolu <ammanabrolu.aditya@gmail.com>
2017-11-24 10:06:24 +0530

    hello comm

--- src/app/admin/admin-input-master.service.ts
+++ src/app/admin/admin-input-master.service.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -51,29 +51,3 @@ export class InputMasterService {
 }
 
 export interface IUser { user: string; html: string }
-=======
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-import { Data } from "@angular/router/router";
-import { Observable } from "rxjs/Observable";
-import { options } from "./headers";
-
-@Injectable()
-export class InputMasterService {
-
-    constructor(private http: Http) {
-
-    }
-
-
-    putInputData(inputdata) {
-
-        console.log("inputdata", inputdata);
-        return this.http.post("http://localhost:3000/saveTemplate-AdminInputMaster", inputdata, options).map(res => res.text()).subscribe(data => console.log(data));
-
-
-    }
-
-}
->>>>>>> here/master
--- src/app/admin/headers.ts
+++ src/app/admin/headers.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Headers, RequestOptions } from '@angular/http';
 
 export let options = new RequestOptions();
@@ -11,14 +11,5 @@ headers.append('Access-Control-Allow-Origin', '*');
 // headers.append('Access-Control-Allow-Methods', 'POST');
 
 //Access-Control-Allow-Methods
-=======
-import { Headers, RequestOptions } from '@angular/http';
 
-export let options = new RequestOptions();
-// let url = 'http://localhost:8081/auth/v1/ldap-login';
-let headers = new Headers();
-headers.append('Content-Type', 'application/json');
-headers.append('Accept', 'application/json');
-headers.append('Access-Control-Allow-Origin', '*');
->>>>>>> here/master
 options.headers = headers;
