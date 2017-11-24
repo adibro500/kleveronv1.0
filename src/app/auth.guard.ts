ffd623190e9d9e00c6487101950cae17cf51e641
Aditya Ammanabrolu <ammanabrolu.aditya@gmail.com>
2017-11-24 10:36:16 +0530

    comms

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -7,14 +7,14 @@ import { Router } from '@angular/router';
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private authGuardService: AuthGuardService, private router: Router) { }
-    canActivate(
-        next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        // this.router.navigate(['/']);
-        if (this.authGuardService.getUserLoggedIn() == false) {
-            alert("Login First");
-            this.router.navigate(['/login']);
-        }
-        console.log("you are user");
-        return this.authGuardService.getUserLoggedIn();
-    }
+    // canActivate(
+    // next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    // //     // this.router.navigate(['/']);
+    // //     if (this.authGuardService.getUserLoggedIn() == false) {
+    // //         alert("Login First");
+    // //         this.router.navigate(['/login']);
+    // //     }
+    // //     console.log("you are user");
+    //      return this.authGuardService.getUserLoggedIn();
+    //  }
 }
